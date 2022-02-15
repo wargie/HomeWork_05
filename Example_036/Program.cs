2b11962b0c821a3042fe69daafc3c0bc35281eee
wargie <wargie@bk.ru>
2022-02-15 12:16:10 +0200

    Оптимизирован код к задаче №036

--- Example_036/Program.cs
+++ Example_036/Program.cs
@@ -1,31 +1,38 @@
 ﻿// 36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных и четных чисел.
  
-int sizeArray = 20;
+int sizeArray = new Random().Next(2, 30);
 
 int[] FillArray(int sizeArray)
-{
-    int[] array = new int[sizeArray];
-    for (int i = 0; i < sizeArray; i++)
+    
     {
-        array[i] = new Random().Next(100, 1000);
+        int[] array = new int[sizeArray];
+
+        for (int i = 0; i < sizeArray; i++)
+
+            {
+                array[i] = new Random().Next(100, 1000);
+            }
+
+        return array;
     }
-    return array;
-}
 
 void PrintResult(int[] array)
-{
-    int even = 0;
-    int uneven = 0;
-    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
-        if (array[i] % 2 == 0) even++;
-        else uneven++;
+        int evenNum = 0;
+        int unevenNum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+
+            {
+                Console.Write($"{array[i]} ");
+                if (array[i] % 2 == 0) evenNum++;
+                else unevenNum++;
+            }
+
+        Console.WriteLine();
+        Console.WriteLine($"Количество нечетных чисел в массиве из {sizeArray} элементов: {unevenNum}");
+        Console.WriteLine($"Количество четных чисел в массиве из {sizeArray} элементов: {evenNum}");
+        
     }
-    Console.WriteLine();
-    Console.WriteLine($"Количество нечетных чисел в массиве: {uneven}");
-    Console.WriteLine($"Количество четных чисел в массиве: {even}");
-    
-}
 
 PrintResult(FillArray(sizeArray));
