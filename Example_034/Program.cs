f32f0a058a03cd322a62517333c7e0aa2ea47d7c
wargie <wargie@bk.ru>
2022-02-15 07:08:18 +0200

    Дополнен код к задаче №34

--- Example_034/Program.cs
+++ Example_034/Program.cs
@@ -1,37 +1,46 @@
 ﻿// 34. Написать программу замену элементов массива на противоположные
 
-// int n = new Random().Next(-100, 101);
-int n = 20;
+int size = new Random().Next(1, 20);
 
-int[] CreateArray(int n)
-{
-    int[] array = new int[n];
-    for (int i = 0; i < n; i++)
+int[] CreateArray(int size)
+    
     {
-        array[i] = new Random().Next(-100, 101);
-    }
-    return array;
-}
+        int[] array = new int[size];
+        
+        for (int i = 0; i < size; i++)
+        
+        {
+            array[i] = new Random().Next(-100, 101);
+        }
 
-int[] GetOppositArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = array[i] * (-1);
+        return array;
     }
-    return array;
-}
+
+int[] GetOppArray(int[] array)
+    
+    {
+        for (int i = 0; i < array.Length; i++)
+        
+        {
+            array[i] = array[i] * (-1);
+        }
+
+        return array;
+    }
 
 void PrintArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
+    
     {
-        System.Console.Write($"{arr[i]} ");
-    }
-    System.Console.WriteLine();
-}
+        for (int i = 0; i < arr.Length; i++)
+        
+        {
+            System.Console.Write($"{arr[i]} ");
+        }
 
-int[] array = CreateArray(n);
+        System.Console.WriteLine();
+    }
+
+int[] array = CreateArray(size);
 
 PrintArray(array);
-PrintArray(GetOppositArray(array));
+PrintArray(GetOppArray(array));
